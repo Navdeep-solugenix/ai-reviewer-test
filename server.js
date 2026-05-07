4784dc1833b5c04a5df1c8525ea0764e5cbbf585
Navdeep-solugenix <navdeep.singh@solugenix.com>
2026-05-07 23:37:43 +0530

    added comment

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/users/:id", (req, res) => {
   res.json(user);
 });
 app.get("/users", (req, res) => {
-  res.json(users);
+  res.json(users); // Return all users
 });
 
 app.post("/users", (req, res) => {
